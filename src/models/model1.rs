5d92fe7a2ee34b0ea3eb6a6078b340ba057e2be4
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2023-08-24 10:52:55 -0300

    minor fix

--- src/models/model1.rs
+++ src/models/model1.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-pub fn model1() -> SolModel {
+pub fn model() -> SolModel {
     let mut ret = SolModel {
         start_addr: 2999,
         end_addr: 3003,
@@ -11,8 +11,8 @@ pub fn model1() -> SolModel {
     ret.data.push(SDataTypes::SolisU16(Point { name: "Product Model", offset: 0, length: 1, write_access: false, value: 0 } ));
     ret.data.push(SDataTypes::SolisU16(Point { name: "DSP software version", offset: 1, length: 1, write_access: false, value: 0 } ));
     ret.data.push(SDataTypes::SolisU16(Point { name: "LCD software version", offset: 2, length: 1, write_access: false, value: 0 } ));
-    ret.data.push(SDataTypes::SolisU32(Point { name: "AC output type", offset: 3, length: 1, write_access: false, value: 0 } ));
-    ret.data.push(SDataTypes::SolisU32(Point { name: "DC output type", offset: 4, length: 1, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU16(Point { name: "AC output type", offset: 3, length: 1, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU16(Point { name: "DC output type", offset: 4, length: 1, write_access: false, value: 0 } ));
 
     ret
 }
