b17ea6a3d412ef2c4ff18405fff211aea28e608a
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2023-08-24 09:35:46 -0300

    First Solis data model.

--- src/models/model1.rs
+++ src/models/model1.rs
@@ -2,16 +2,17 @@ use super::*;
 
 pub fn model1() -> SolModel {
     let mut ret = SolModel {
-        start_addr: 30000,
-        end_addr: 30034,
+        start_addr: 2999,
+        end_addr: 3003,
         model_number: 1,
-        qtd: 35,
+        qtd: 5,
         data: Vec::new(),
     };
-    ret.data.push(SDataTypes::SolisU16(Point { name: "Model ID", offset: 0, length: 1, write_access: false, value: 0 } ));
-    ret.data.push(SDataTypes::SolisU16(Point { name: "Number of PV strings", offset: 1, length: 1, write_access: false, value: 0 } ));
-    ret.data.push(SDataTypes::SolisU16(Point { name: "Number of MPPT trackers", offset: 2, length: 1, write_access: false, value: 0 } ));
-    ret.data.push(SDataTypes::SolisU32(Point { name: "Rated Power", offset: 3, length: 2, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU16(Point { name: "Product Model", offset: 0, length: 1, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU16(Point { name: "DSP software version", offset: 1, length: 1, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU16(Point { name: "LCD software version", offset: 2, length: 1, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU32(Point { name: "AC output type", offset: 3, length: 1, write_access: false, value: 0 } ));
+    ret.data.push(SDataTypes::SolisU32(Point { name: "DC output type", offset: 4, length: 1, write_access: false, value: 0 } ));
 
     ret
 }
