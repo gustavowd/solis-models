4b4e9196892f84a4e9bcee95b60a4d1728a0a259
Gustavo Weber Denardin <gustavo.denardin@gmail.com>
2023-12-06 14:37:09 -0300

    minor fix

--- src/models/model6_4x.rs
+++ src/models/model6_4x.rs
@@ -3,10 +3,10 @@ use super::*;
 pub fn model() -> SolModel {
     let mut ret = SolModel {
         start_addr: 3050,
-        end_addr: 3052,
+        end_addr: 3053,
         reg_types: 4,
         model_number: 6,
-        qtd: 3,
+        qtd: 4,
         data: Vec::new(),
     };
     ret.data.push(SDataTypes::SolisI16(Point { name: "Reactive power limitation", offset: 0, length: 1, write_access: true, value: 0 } ));
